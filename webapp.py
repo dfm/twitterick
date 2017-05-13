6e4542014d36f53b2f7da9fe79e4d8bfab7b69aa
Dan F-M <danfm@nyu.edu>
2017-05-12 20:18:11 -0700

    host

--- webapp.py
+++ webapp.py
@@ -202,7 +202,7 @@ def main():
     parse_command_line()
 
     http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(options.port, address="127.0.0.1")
+    http_server.listen(options.port, address="0.0.0.0")
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
